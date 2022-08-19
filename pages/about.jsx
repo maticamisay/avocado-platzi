8ab796f97e289def46fb41988fa26a71803d4ceb
maticamisay <maticamisay99@hotmail.com>
2022-08-19 20:51:45 -0300

    Initial commit

---
+++ pages/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+        <h1>Esta es la pagina de about</h1>
+    </div>
+  )
+}
+
+export default about
---
+++ pages/product/[id].js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductItem = () => {
+  return (
+    <div>
+        Esta la pagina de prueba del producto
+    </div>
+  )
+}
+
+export default ProductItem
