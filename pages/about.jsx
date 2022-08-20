666b24abb53a167ddb8494fea7bae81151994164
maticamisay <maticamisay99@hotmail.com>
2022-08-19 21:34:06 -0300

    navbar

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
 
 const about = () => {
   return (
     <div>
+      <Navbar />
         <h1>Esta es la pagina de about</h1>
     </div>
   )
